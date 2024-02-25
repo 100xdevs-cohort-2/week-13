6b460cb7f29a3eae1f82f55ba2227439906bb77c
Kirat <harkirat96@gmail.com>
2024-02-25 12:51:44 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,16 +12,4 @@ export const app = new Hono<{
 app.route('/api/v1/user', userRouter)
 app.route('/api/v1/book', bookRouter)
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
-
 export default app
